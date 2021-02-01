60365e2b30acb25ed4d4bfcaeec24d75a60a71ca
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-01 17:52:15 +0900

    Revert "Merge branch 'master' into second_training"
    This reverts commit 64267a05c4c6422f626572b595a5b411344a9dcc, reversing
    changes made to 4136469379351fdaddab61f69de7628255f269f2.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -40,9 +40,6 @@ class CalendarsController < ApplicationController
         wday_num = wday_num -7
       end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num + x]}
-
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
-
       @week_days.push(days)
     end
 
